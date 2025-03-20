85b242f2e247616a2b7016b8f558cc38f650c6ca
Han-Bin Song <thdgksq@naver.com>
2025-03-20 18:20:55 +0900

    Order 페이지 UI 생성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "react";
-import "./App.css";
 import SummaryPage from "./pages/SummaryPage";
+import OrderPage from "./pages/OrderPage";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div style={{ padding: "4rem" }}>
+      <OrderPage />
       <SummaryPage />
-    </>
+    </div>
   );
 }
 
